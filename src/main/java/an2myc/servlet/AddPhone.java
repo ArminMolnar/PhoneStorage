3f9fe6969518958612c919b769927bc67834bd88
ArminMolnar <molnar.armin9@gmail.com>
2025-04-01 19:53:52 +0200

    implement saving new phone logic

--- src/main/java/an2myc/servlet/AddPhone.java
+++ src/main/java/an2myc/servlet/AddPhone.java
@@ -1,5 +1,6 @@
 package an2myc.servlet;
 
+import an2myc.service.PhoneService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,13 +10,23 @@ import java.io.IOException;
 
 public class AddPhone extends HttpServlet {
 
+    private static final PhoneService phoneService = new PhoneService();
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             req.getRequestDispatcher("addPhone.jsp").forward(req, resp);
     }
 
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp){
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.setCharacterEncoding("UTF-8");
+
+        var manufacturer = req.getParameter("manufacturer");
+        var type = req.getParameter("type");
+        var imei = req.getParameter("imei");
+
+        phoneService.save(null, manufacturer, type, imei);
+        resp.sendRedirect(req.getContextPath() + "/listServlet");
 
     }
 }
