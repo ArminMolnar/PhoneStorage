d06f60ecba20bb99c1aeb50390a565462951e682
ArminMolnar <molnar.armin9@gmail.com>
2025-04-01 19:22:37 +0200

    add servlet for adding phone

---
+++ src/main/java/an2myc/servlet/AddPhone.java
@@ -0,0 +1,18 @@
+package an2myc.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddPhone extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+}
