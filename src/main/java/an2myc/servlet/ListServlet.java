f47502a8d5f30d0da16c2ce3b5d01574be5d0b1c
ArminMolnar <molnar.armin9@gmail.com>
2025-04-01 18:18:42 +0200

    create empty ListServlet

---
+++ src/main/java/an2myc/servlet/ListServlet.java
@@ -0,0 +1,19 @@
+package an2myc.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ListServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+
+}
