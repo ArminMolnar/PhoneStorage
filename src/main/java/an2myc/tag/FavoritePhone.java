3c85ed39cd0372cac09b33ed6fa6820b1e7a2878
ArminMolnar <molnar.armin9@gmail.com>
2025-04-01 21:08:22 +0200

    fix for favorite phone display

--- src/main/java/an2myc/tag/FavoritePhone.java
+++ src/main/java/an2myc/tag/FavoritePhone.java
@@ -17,18 +17,20 @@ public class FavoritePhone extends SimpleTagSupport {
     public void doTag() throws IOException {
         var context = (PageContext) this.getJspContext();
         var request = (HttpServletRequest) context.getRequest();
-        JspWriter out = this.getJspContext().getOut();
+        JspWriter out = getJspContext().getOut();
 
-        String phoneId = request.getParameter("id");
+        String phoneId = (String) request.getAttribute("id");
+
         if (phoneId == null) {
             phoneId = getCookieValue(request.getCookies(), "id");
         }
 
-        if (phoneId != null) {
-            var phone = phoneService.findById(Long.parseLong(phoneId));
-            out.println(phone != null ? "Favorite phone: " + phone.getManufacturer() + " " + phone.getType() + " " + phone.getImei() : "You don't have a favorite phone");
+            if (phoneId != null) {
+                var phone = phoneService.findById(Long.parseLong(phoneId));
+                out.println(phone != null ? "Favorite phone: " + phone.getManufacturer() + " " + phone.getType() + " " + phone.getImei() : "You don't have a favorite phone");
+            }
         }
-    }
+
 
     private String getCookieValue(Cookie[] cookies, String cookieName) {
         if (cookies != null) {
