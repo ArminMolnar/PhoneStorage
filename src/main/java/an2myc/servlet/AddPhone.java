5b36ba09c3184d2fec3ed719c4de13ac026f9d8d
ArminMolnar <molnar.armin9@gmail.com>
2025-04-01 19:24:53 +0200

    map addPhone.jsp to AddPhone servlet

--- src/main/java/an2myc/servlet/AddPhone.java
+++ src/main/java/an2myc/servlet/AddPhone.java
@@ -1,14 +1,17 @@
 package an2myc.servlet;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 public class AddPhone extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp){
-
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            req.getRequestDispatcher("addPhone.jsp").forward(req, resp);
     }
 
     @Override
